7a81b5c570edef3a2f39be4d2b2b61cebced04fd
nmar30 <nmarcisso@live.com>
2021-06-04 12:50:07 -0400

    Moved Collections to its own component from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import axios from './axios'
+import React from 'react';
+import Collections from './components/Collections/Collections'
 
 function App() {
-    const [collections , setCollections] = useState([]);
-
-    useEffect(() => {
-        async function fetchData() {
-            const request = await axios.get(`collection/`);
-            setCollections(request.data)
-            return request
-        }
-        fetchData()
-    }, [])
-
-    console.log(collections)
 
     return (
         <div>
-
+            <Collections />
         </div>
     )
 }
